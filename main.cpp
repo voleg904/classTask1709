786bde805ddd51747ed9af745246d9b51197a2de
Oleg <n7o.nto@yandex.ru>
2026-09-17 21:46:30 +0300

    Copied the code from task

--- main.cpp
+++ main.cpp
@@ -1,6 +1,41 @@
 #include <iostream>
+#include <chrono>
+#include <vector>
+
+class Clicker
+{
+  public:
+    Clicker():
+      start_(std::chrono::high_resolution_clock::now())
+    {}
+    double millisec() const
+    {
+      using std::chrono::high_resolution_clock;
+      using std::chrono::duration_cast;
+      using std::chrono::milliseconds;
+      auto t = high_resolution_clock::now();
+      return duration_cast< milliseconds >(t - start_).count();
+    }
+  private:
+    std::chrono::time_point< std::chrono::system_clock > start_;
+};
+
+using data_t = std::vector< unsigned long long >;
+using value_t = data_t::value_type;
 
 int main()
 {
-  return 0;
+  constexpr size_t size{1'000'000'000};
+  double init{0}, total{0};
+  value_t sum{0};
+  {
+    Clicker cl;
+    data_t values(size, 1);
+    init = cl.millisec();
+    for (size_t i = 0; i < values.size(); ++i)
+    {
+      sum += values[i];
+    }
+    total = cl.millisec();
+  }
 }
