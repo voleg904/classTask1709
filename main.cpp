045f0cb8a33b781a80c089d601207080e0f124e5
Oleg <n7o.nto@yandex.ru>
2026-09-17 23:01:11 +0300

    Added sum func

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <chrono>
 #include <vector>
+#include <future>
 
 class Clicker
 {
@@ -17,14 +18,29 @@ class Clicker
       return duration_cast< milliseconds >(t - start_).count();
     }
   private:
-    std::chrono::time_point< std::chrono::system_clock > start_;
+    std::chrono::time_point< std::chrono::high_resolution_clock > start_;
 };
 
 using data_t = std::vector< unsigned long long >;
 using value_t = data_t::value_type;
 
-int main()
+value_t sum_thread(const value_t* data, size_t starе, size_t block)
 {
+  value_t sum = 0;
+  for (size_t i = 0; i < block; ++i)
+  {
+    sum += data[start + i];
+  }
+  return sum;
+}
+
+int main(int argc, char* argv[])
+{
+  if (argc != 2)
+  {
+    return 1;
+  }
+  const size_t threads = std::stoull(argv[1]);
   constexpr size_t size{1'000'000'000};
   double init{0}, total{0};
   value_t sum{0};
@@ -32,10 +48,8 @@ int main()
     Clicker cl;
     data_t values(size, 1);
     init = cl.millisec();
-    for (size_t i = 0; i < values.size(); ++i)
-    {
-      sum += values[i];
-    }
-    total = cl.millisec();
+
+    
   }
+  return 0;
 }
